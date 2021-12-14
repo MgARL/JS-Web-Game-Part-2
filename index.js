59ad5a7e787cb6b5633741b31aa75d7d7dd2099f
Miguel(MgARL) <mgarlcr@gmail.com>
2021-12-13 19:25:28 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//global Variables
+let inventory
+
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -10,10 +13,16 @@ function newImage(url, left, bottom){
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', () => {
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
 
 function newInventory(){
-    let inventory = document.createElement('div')
+    inventory =  document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
@@ -26,9 +35,11 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return inventory
 }
 
 newInventory()
+
 newImage('assets/green-character.gif', 100, 250)
 newImage('assets/tree.png', 200, 450)
 newImage('assets/pillar.png', 350, 250)
